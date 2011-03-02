a5f4883d7134b9d6700d8ea8ec57752aa45b7ad8
redrocketracoon@googlemail.com <redrocketracoon@googlemail.com@c048aa3c-d72a-36f6-f25d-3a43fe95c646>
2011-03-02 13:31:22 +0000

    added new cleaner implementation to save and retrieve Settings in WebSMSsend. It's not yet integrated into WebSMSsend!
    git-svn-id: https://websmssend.googlecode.com/svn/trunk@165 c048aa3c-d72a-36f6-f25d-3a43fe95c646

---
+++ webSMSsend/WebSMSsend/src/Storage/AppSettings.java
@@ -0,0 +1,136 @@
+/*
+ *
+ *
+Copyright 2011 redrocketracoon@googlemail.com
+This file is part of WebSMSsend.
+
+WebSMSsend is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+WebSMSsend is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with WebSMSsend.  If not, see <http://www.gnu.org/licenses/>.
+
+ *
+ *
+ */
+package Storage;
+
+/**
+ * Stores all App specific settings
+ * @author redrocketracoon@googlemail.com
+ */
+public class AppSettings {
+
+    private AppSettingsManager appSettingsManager;
+    private int activeAccount;
+    private boolean debug;
+    private boolean autoUpdate;
+    private boolean saveEachCharacter;
+    private String tempSmsText;
+    private String tempSmsTo;
+
+    public AppSettings(AppSettingsManager appSettingsManager) {
+        this.appSettingsManager = appSettingsManager;
+    }
+
+    /**
+     * @return the activeAccount
+     */
+    public int getActiveAccount() {
+        return activeAccount;
+    }
+
+    /**
+     * @param activeAccount the activeAccount to set
+     */
+    public void setActiveAccount(int activeAccount) {
+        this.activeAccount = activeAccount;
+        changed();
+    }
+
+    /**
+     * @return the debug
+     */
+    public boolean isDebug() {
+        return debug;
+    }
+
+    /**
+     * @param debug the debug to set
+     */
+    public void setDebug(boolean debug) {
+        this.debug = debug;
+        changed();
+    }
+
+    /**
+     * @return the autoUpdate
+     */
+    public boolean isAutoUpdate() {
+        return autoUpdate;
+    }
+
+    /**
+     * @param autoUpdate the autoUpdate to set
+     */
+    public void setAutoUpdate(boolean autoUpdate) {
+        this.autoUpdate = autoUpdate;
+        changed();
+    }
+
+    /**
+     * @return the saveEachCharacter
+     */
+    public boolean isSaveEachCharacter() {
+        return saveEachCharacter;
+    }
+
+    /**
+     * @param saveEachCharacter the saveEachCharacter to set
+     */
+    public void setSaveEachCharacter(boolean saveEachCharacter) {
+        this.saveEachCharacter = saveEachCharacter;
+        changed();
+    }
+
+    /**
+     * @return the tempSmsText
+     */
+    public String getTempSmsText() {
+        return tempSmsText == null ? "" : tempSmsText;
+    }
+
+    /**
+     * @param tempSmsText the tempSmsText to set
+     */
+    public void setTempSmsText(String tempSmsText) {
+        this.tempSmsText = tempSmsText;
+        changed();
+    }
+
+    /**
+     * @return the tempSmsTo
+     */
+    public String getTempSmsTo() {
+        return tempSmsTo == null ? "" : tempSmsTo;
+    }
+
+    /**
+     * @param tempSmsTo the tempSmsTo to set
+     */
+    public void setTempSmsTo(String tempSmsTo) {
+        this.tempSmsTo = tempSmsTo;
+        changed();
+    }
+
+    private void changed(){
+        appSettingsManager.update(this);
+    }
+}
---
+++ webSMSsend/WebSMSsend/src/Storage/AppSettingsManager.java
@@ -0,0 +1,122 @@
+/*
+ *
+ *
+Copyright 2011 redrocketracoon@googlemail.com
+This file is part of WebSMSsend.
+
+WebSMSsend is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+WebSMSsend is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with WebSMSsend.  If not, see <http://www.gnu.org/licenses/>.
+
+ *
+ *
+ */
+package Storage;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+/**
+ *
+ * @author redrocketracoon@googlemail.com
+ */
+public class AppSettingsManager extends StorageManager {
+
+    private AppSettings appSettings;
+
+    public AppSettingsManager() {
+        this.recordStoreName = "AppSettings";
+    }
+
+    public AppSettings getAppSettings() {
+        if (appSettings == null) {
+            readAppSettings();
+        }
+        return appSettings;
+    }
+
+    public void update(AppSettings changedAppSettings) {
+        try {
+            saveAppSettings();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void readAppSettings() {
+        try {
+            byte[] data = this.getData();
+            changeAppSettingsFromByteArray(data);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private boolean saveAppSettings() {
+        boolean saved = false;
+        if (appSettings != null) {
+            try {
+                byte[] data = changeAppSettingsToByteArray();
+                this.saveData(data);
+                saved = true;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return saved;
+    }
+
+    private byte[] changeAppSettingsToByteArray() {
+        byte[] data = null;
+
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            DataOutputStream dos = new DataOutputStream(baos);
+
+            dos.writeInt(appSettings.getActiveAccount());
+            dos.writeBoolean(appSettings.isAutoUpdate());
+            dos.writeBoolean(appSettings.isDebug());
+            dos.writeBoolean(appSettings.isSaveEachCharacter());
+            dos.writeUTF(appSettings.getTempSmsText());
+            dos.writeUTF(appSettings.getTempSmsTo());
+
+            baos.close();
+            dos.close();
+            data = baos.toByteArray();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return data;
+    }
+
+    private void changeAppSettingsFromByteArray(byte[] data) {
+        try {
+            ByteArrayInputStream bais = new ByteArrayInputStream(data);
+            DataInputStream dis = new DataInputStream(bais);
+
+            appSettings = new AppSettings(this);
+            appSettings.setActiveAccount(dis.readInt());
+            appSettings.setAutoUpdate(dis.readBoolean());
+            appSettings.setDebug(dis.readBoolean());
+            appSettings.setSaveEachCharacter(dis.readBoolean());
+            appSettings.setTempSmsText(dis.readUTF());
+            appSettings.setTempSmsTo(dis.readUTF());
+
+            bais.close();
+            dis.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
