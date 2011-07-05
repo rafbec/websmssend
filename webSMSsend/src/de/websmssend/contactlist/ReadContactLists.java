0d8d90724e045bf6d04e304f2a4e3566a979c844
redrocketracoon@gmail.com <redrocketracoon@gmail.com@c048aa3c-d72a-36f6-f25d-3a43fe95c646>
2011-07-05 17:38:35 +0000

    fixed bug where websmssend crashed if user doesn't authorize phonebook access
    git-svn-id: https://websmssend.googlecode.com/svn/trunk@296 c048aa3c-d72a-36f6-f25d-3a43fe95c646

--- webSMSsend/src/de/websmssend/contactlist/ReadContactLists.java
+++ webSMSsend/src/de/websmssend/contactlist/ReadContactLists.java
@@ -93,122 +93,127 @@ public class ReadContactLists extends List implements Runnable, CommandListener 
         String conForName;
         String[] conAllNames;
         String[] conPhoneNummbers;
-        String[] lists = pim.listPIMLists(PIM.CONTACT_LIST);
-        for (int i = 0; i < lists.length; i++) {
-            try {
-                pimlist = pim.openPIMList(PIM.CONTACT_LIST, PIM.READ_ONLY, lists[i]);
-            } catch (Exception e) {
-            }
+        try {
+            String[] lists = pim.listPIMLists(PIM.CONTACT_LIST);
 
-            try {
-                enumeration = pimlist.items();
-            } catch (Exception e) {
-            }
 
-            while (enumeration.hasMoreElements()) {
-                contact = (Contact) enumeration.nextElement();
-                conItem = new ContactItem();
+            for (int i = 0; i < lists.length; i++) {
                 try {
-                    conName = "";
-                    conForName = "";
-                    conAllNames = new String[0];
+                    pimlist = pim.openPIMList(PIM.CONTACT_LIST, PIM.READ_ONLY, lists[i]);
+                } catch (Exception e) {
+                }
 
+                try {
+                    enumeration = pimlist.items();
+                } catch (Exception e) {
+                }
+
+                while (enumeration.hasMoreElements()) {
+                    contact = (Contact) enumeration.nextElement();
+                    conItem = new ContactItem();
                     try {
-                        //read all Names
+                        conName = "";
+                        conForName = "";
+                        conAllNames = new String[0];
+
                         try {
-                            conAllNames = contact.getStringArray(Contact.NAME, Contact.ATTR_NONE);
+                            //read all Names
+                            try {
+                                conAllNames = contact.getStringArray(Contact.NAME, Contact.ATTR_NONE);
+                            } catch (Exception e) {
+                                debug("ATTR_NONE: " + e.getMessage(), conItem);
+                            }
+
+                            if (conAllNames.length > Contact.NAME_FAMILY) {
+                                conName = conAllNames[Contact.NAME_FAMILY];
+                            }
+
+                            if (conAllNames.length > Contact.NAME_GIVEN) {
+                                conForName = conAllNames[Contact.NAME_GIVEN];
+                            }
+
+                            debug("Anzahl: " + conAllNames.length, conItem);
+
+                            for (int y = 0; y < conAllNames.length; y++) {
+                                if (y < NAME_FIELDS.length) {
+                                    debug(NAME_FIELDS[y] + ": " + conAllNames[y], conItem);
+                                } else {
+                                    debug("PROPRIETARY: " + conAllNames[y], conItem);
+                                }
+                            }
                         } catch (Exception e) {
-                            debug("ATTR_NONE: " + e.getMessage(),conItem);
+                            debug("Fehler beim Einlesen des Namens:\r\n" + e.getMessage(), conItem);
                         }
 
-                        if (conAllNames.length > Contact.NAME_FAMILY) {
-                            conName = conAllNames[Contact.NAME_FAMILY];
+                        if (conName == null) {
+                            conName = "";
+                        }
+                        if (conForName == null) {
+                            conForName = "";
                         }
 
-                        if (conAllNames.length > Contact.NAME_GIVEN) {
-                            conForName = conAllNames[Contact.NAME_GIVEN];
-                        }
-
-                        debug("Anzahl: " + conAllNames.length,conItem);
-
-                        for (int y = 0; y < conAllNames.length; y++) {
-                            if (y < NAME_FIELDS.length) {
-                                debug(NAME_FIELDS[y]+": "+conAllNames[y],conItem);
+                        if (conName.length() != 0) {
+                            if (conForName.length() != 0) {
+                                conItem.setName(conName + ", " + conForName);
                             } else {
-                                debug("PROPRIETARY: "+conAllNames[y],conItem);
+                                conItem.setName(conName);
+                            }
+                        } else if (conForName.length() != 0) {
+                            conItem.setName(conForName);
+                        } else { //Backup Method find first non null value and set as name
+                            for (int y = 0; y < conAllNames.length; y++) {
+                                if (conAllNames[y].length() != 0) {
+                                    conItem.setName(conAllNames[y]);
+                                    debug("AnyName: " + conItem.getName(), conItem);
+                                    break;
+                                }
+                            }
+
+                            //if all names are null try Organisation field
+                            if (conItem.getName().length() == 0) {
+                                try {
+                                    if (pimlist.isSupportedField(Contact.ORG)) {
+                                        conItem.setName(contact.getString(Contact.ORG, Contact.ATTR_NONE));
+                                    }
+                                    debug("Organisation: " + conItem.getName(), conItem);
+                                } catch (Exception ex) {
+                                    debug("Organisation: " + ex.getMessage(), conItem);
+                                }
                             }
                         }
                     } catch (Exception e) {
-                        debug("Fehler beim Einlesen des Namens:\r\n" + e.getMessage(),conItem);
+                        debug("ContactItem: " + e.getMessage(), conItem);
                     }
 
-                    if (conName == null) {
-                        conName = "";
-                    }
-                    if (conForName == null) {
-                        conForName = "";
-                    }
-
-                    if (conName.length() != 0) {
-                        if (conForName.length() != 0) {
-                            conItem.setName(conName + ", " + conForName);
-                        } else {
-                            conItem.setName(conName);
+                    int phoneNos = contact.countValues(Contact.TEL);
+                    try {
+                        conPhoneNummbers = new String[phoneNos];
+                        for (int y = 0; y < phoneNos; y++) {
+                            conPhoneNummbers[y] = contact.getString(contact.TEL, y);
                         }
-                    } else if (conForName.length() != 0) {
-                        conItem.setName(conForName);
-                    } else { //Backup Method find first non null value and set as name
-                        for (int y = 0; y < conAllNames.length; y++) {
-                            if (conAllNames[y].length() !=0) {
-                                conItem.setName(conAllNames[y]);
-                                debug("AnyName: " + conItem.getName(), conItem);
-                                break;
+                        conItem.setPhoneNumbers(conPhoneNummbers);
+                    } catch (Exception e) {
+                    }
+
+                    if (conItem.getName().equals("")) { //if no name could be found show first phone number
+                        if (conItem.getPhoneNumbers() != null) {
+                            if (conItem.getPhoneNumbers().length > 0) {
+                                append("" + conItem.getPhoneNumbers()[0], null);
+                                contacts.addElement(conItem);
                             }
+                        } else { //ignore contact since no information could be retrieved
                         }
-                        
-                        //if all names are null try Organisation field
-                        if (conItem.getName().length() == 0){
-                            try{
-                               if(pimlist.isSupportedField(Contact.ORG)){
-                                   conItem.setName(contact.getString(Contact.ORG, Contact.ATTR_NONE));
-                               }
-                               debug("Organisation: " + conItem.getName(), conItem);
-                            }catch(Exception ex){
-                                debug("Organisation: " + ex.getMessage(),conItem);
-                            }
-                        }
+                    } else {
+                        append(conItem.getName() + "", null);
+                        contacts.addElement(conItem);
                     }
-                } catch (Exception e) {
-                    debug("ContactItem: " + e.getMessage(),conItem);
-                }
-
-                int phoneNos = contact.countValues(Contact.TEL);
-                try {
-                    conPhoneNummbers = new String[phoneNos];
-                    for (int y = 0; y < phoneNos; y++) {
-                        conPhoneNummbers[y] = contact.getString(contact.TEL, y);
-                    }
-                    conItem.setPhoneNumbers(conPhoneNummbers);
-                } catch (Exception e) {
-                }
-
-                if (conItem.getName().equals("")) { //if no name could be found show first phone number
-                    if (conItem.getPhoneNumbers() != null) {
-                        if (conItem.getPhoneNumbers().length > 0) {
-                            append("" + conItem.getPhoneNumbers()[0], null);
-                            contacts.addElement(conItem);
-                        }
-                    } else { //ignore contact since no information could be retrieved
-                    }
-                } else {
-                    append(conItem.getName() + "", null);
-                    contacts.addElement(conItem);
                 }
             }
+        } catch (Exception e) {
         }
     }
 
-    private void debug(String debugMessage, ContactItem contact){
+    private void debug(String debugMessage, ContactItem contact) {
         contact.addDebugMsg(debugMessage);
     }
 
